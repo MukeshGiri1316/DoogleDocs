afd262e6d3fb64844423d9c0708a2cbdb58fecf8
Mukesh Giri <girimukesh14316@gmail.com>
2026-07-12 23:16:43 +0530

    fix prisma issue

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverExternalPackages: ["@prisma/client", "pg"],
+  },
 };
 
 export default nextConfig;
